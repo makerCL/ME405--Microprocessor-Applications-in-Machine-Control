af4a5dd7089b35739dc36b5b1c948a1204e67278
cerlenbo <123514149+cerlenbo@users.noreply.github.com>
2023-01-31 13:45:04 -0800

    Lab 2 ver 1.0
    initial version of motor_driver function

--- lab2/src/feedback_control.py
+++ lab2/src/feedback_control.py
@@ -1,26 +1,49 @@
-'''
+
+'''!
 
 
 '''
-
-
 class FeedbackControl:
-    '''
-    @param k_p
-    @param setpoint
-    @param time_step- in seconds
+    '''!
+    @param k_p The controller proportional gain [PWM/count]
+    @param setpoint	The motor setpoint in [stepper counts]
+    @param PWM 
     '''
     def __init__(self, setpoint = 0, k_p = 0):
         self.k_p = k_p
         self.setpoint = setpoint
-
+        self.PWM = 0
     def run(self, current_theta):
-        pass
-
+        self.PWM = self.k_p*(self.setpoint - current_theta)
+        
     def set_setpoint(self, setpoint):
         self.setpoint = setpoint
-
+    def set_kp(self,k_p):
+        self.k_p = k_p
 #### TEST CODE #############################
 
 if __name__ == '__main__':
-    pass
+    import encoder_reader as er
+    import motor_driver as md
+    import pyb
+    
+    en_pin = pyb.Pin.board.PC1
+    in1pin = pyb.Pin.board.PA0
+    in2pin = pyb.Pin.board.PA1
+    timer = 5
+    moe = md.MotorDriver (en_pin, in1pin, in2pin, timer)
+    
+    enc_pin1 = pyb.Pin (pyb.Pin.board.PC6, pyb.Pin.IN)
+    enc_pin2 = pyb.Pin (pyb.Pin.board.PC7, pyb.Pin.IN)
+    timer = 8
+    encd = er.EncoderReader (enc_pin1, enc_pin2, timer)
+    
+    mc = FeedbackControl()
+    mc.set_setpoint(8000)
+    mc.set_kp(0.5)
+    
+    while True:   
+        encd.read()
+        mc.run(encd.position)
+        moe.set_duty_cycle (mc.PWM)
+    

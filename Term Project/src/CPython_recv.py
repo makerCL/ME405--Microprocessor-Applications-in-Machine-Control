1f064ec8a6c4dfbf23c904f343ecf0fea44278ed
makercl <56234088+makerCL@users.noreply.github.com>
2023-03-03 20:00:02 -0800

    heatplot, not updating

--- Term Project/src/CPython_recv.py
+++ Term Project/src/CPython_recv.py
@@ -10,19 +10,22 @@ import serial
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 start = time.time()
 
 port_path = '/dev/cu.usbmodem11103'
 print(f"Attempting Port Receive on {port_path}")
 
-def heatmap():
-    pass
-
+def heatmap(data):
+    hm = sns.heatmap(data, cmap = 'coolwarm')
+    plt.show()
+##TODO: make this update live
+#https://www.geeksforgeeks.org/how-to-update-a-plot-on-same-figure-during-the-loop/
 
 reading_data = False
 
-data = np.empty((0, 32), dtype=float)
+data = np.empty((0, 32), dtype=int)
 
 with serial.Serial (port_path, 115200) as s_port:
     while(True):
@@ -33,20 +36,35 @@ with serial.Serial (port_path, 115200) as s_port:
                 if charIn == b'Data_Start\r\n':
                     reading_data = True
                 elif charIn == b'Data_Stop\r\n':
+                    print(data)  # print the updated data array
+                    #print(data.shape)
+                    print("\n\n\n\n")
                     reading_data = False
-                    heatmap()
-                    data = np.empty((0, 32), dtype=float)
+                    heatmap(data)
+
+                    #Reinitialize array as empty
+                    data = np.empty((0, 32), dtype=int)
+
                 elif reading_data:
                     # decode the bytes to string, remove any whitespace characters, and strip the b and '
-                    charIn = charIn.decode().strip(' ,')[2:-1]
-                    print(charIn)
+                    charIn = charIn.decode().strip(' ,\r\nb')
+                    int_row = []
+                    for x in charIn.split(','):
+                        try:
+                            int_row.append(int(x))
+                        except:
+                            int_row.append(0)
+                    if not len(int_row) == 32:
+                        raise Exception("Row of data was invalid, and thus array will be wrong size to concatenate")
+                        #alternatively could just assign row of zeros.
+
                     # split the line into comma-separated values and convert them to floats
-                    row = np.array([int(x) for x in charIn.split(',')])
+                    np_row = np.array(int_row)
                     
                     # append the row to the data array
-                    data = np.vstack((data, row))
+                    data = np.vstack((data, np_row))
                     
-                    print(data)  # print the updated data array
+            
 
         except KeyboardInterrupt:
             print("Port Closing")
